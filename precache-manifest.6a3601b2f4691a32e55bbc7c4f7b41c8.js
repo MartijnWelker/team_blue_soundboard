eb3e751ae6b4d898501dcd6f6b931f3747bbd5c2
Martijn Welker <martijnwelker@hotmail.com>
2020-04-02 15:28:39 +0200

    Updates

---
+++ precache-manifest.6a3601b2f4691a32e55bbc7c4f7b41c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ace32c1890049ec16f265c3c3ee9d1f8",
+    "url": "/team_blue_soundboard/index.html"
+  },
+  {
+    "revision": "2174fb267c822328742a",
+    "url": "/team_blue_soundboard/static/css/main.44ce3840.chunk.css"
+  },
+  {
+    "revision": "4a35dbefa1c14e087e61",
+    "url": "/team_blue_soundboard/static/js/2.06ae92e0.chunk.js"
+  },
+  {
+    "revision": "f7fe43cf5d12b7c6064f2a7e415de116",
+    "url": "/team_blue_soundboard/static/js/2.06ae92e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2174fb267c822328742a",
+    "url": "/team_blue_soundboard/static/js/main.4e59a7cb.chunk.js"
+  },
+  {
+    "revision": "ef4c45c2d4b58b260020",
+    "url": "/team_blue_soundboard/static/js/runtime-main.dab87c4f.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/MartijnWelker/team_blue_soundboard/precache-manifest.ff93db299c3704a9cbed56abefd37dfc.js"
+  "/team_blue_soundboard/precache-manifest.6a3601b2f4691a32e55bbc7c4f7b41c8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/MartijnWelker/team_blue_soundboard/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/team_blue_soundboard/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
